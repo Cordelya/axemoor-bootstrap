0dd2712dd3f5b5b26e0771c7fd6d157b8b6f31fe
Jacqueline Arsenault <justcordelya@gmail.com>
2020-08-19 15:32:52 -0400

    Modified to make this script run more similarly to the raspbian script

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -5,39 +5,45 @@
 # =================================== #
 
 # First, some housekeeping
-echo "Beginning system update and software install"
-sudo apt update
-sudo apt upgrade -y
+echo "Beginning software update"
+sudo apt-get update
+sudo apt-get upgrade -y
 echo "Software update complete"
-$install1="apache2 git openssh systemctl
-atom"
-$install2="mysql-server php7.4"
-sudo apt install "$install1" -y
+
+sudo apt-get install -y apache2
 
 echo "Done installing Apache2"
+echo "Enable and start SSH"
+sudo systemctl enable ssh
+sudo systemctl start ssh
+echo "Done with SSH"
+
 # Apache Config
 echo "Apache config starting now"
+
+# Check for /var/www. If it isn't found, we can't continue.
+if [ ! -d "/var/www" ];
+  then echo "Apache didn't install correctly. Aborting." && exit
+  else echo "Apache installed correctly. Continuing.."
+fi
+
+# Check for /var/www/axemoor. If it doesn't exist, make it.
+if [ ! -d "/var/www/axemoor" ];
+then
 sudo mkdir /var/www/axemoor/public_html -p
 sudo chown -R $USER:$USER /var/www/axemoor/public_html
 sudo chmod -R 755 /var/www
-sudo touch /etc/apache2/sites-available/axemoor.net.conf
-$newconf="/etc/apache2/sites-available/axemoor.net.conf"
-echo '<VirtualHost *:80>' >> $newconf
-echo '    ServerAdmin webminister@axemoor.net' >> $newconf
-echo '    ServerName axemoor.net' >> $newconf
-echo '    ServerAlias www.axemoor.net' >> $newconf
-echo '    DocumentRoot /var/www/html' >> $newconf
-echo '    ErrorLog ${APACHE_LOG_DIR}/error.log' >> $newconf
-echo '    CustomLog ${APACHE_LOG_DIR}/access.log combined' >> $newconf
-echo '</VirtualHost>' >> $newconf
-sudo a2ensite axemoor.net.conf
-sudo a2dissite 000-default.conf
-echo "Attempting to restart Apache. Hang on."
-sudo systemctl restart apache2
-#echo "Apache status is"
-#sudo systemctl status apache2
+fi
+echo "Attempting to restart Apache. Hang on!"
+
+sudo systemctl restart apache2 --no-pager
+sudo systemctl status apache2 --no-pager
 echo "Done configuring Apache. Now installing additional software."
-sudo apt install "$install2" -y
+sudo apt-get install -y php
+sudo apt-get install -y git
+sudo apt-get install -y mysql-server
+sudo apt-get install -y php-mysql
+sudo service apache2 restart
 # git Config
 read -p "Enter your name (may use SCA name here):" name
 git config --global user.name "$name"
@@ -45,13 +51,24 @@ read -p "Enter your email address (DO NOT use webminister@axemoor.net):" email
 git config --global user.email "$email"
 
 # clone the Axemoor website repo
-cd /var/www/axemoor/public_html
-git clone https://github.com/Axemoor/website.git .
+git clone https://github.com/Axemoor/website.git /var/www/axemoor/public_html
 
 # clone the Axemoor help system
-mkdir ~/help
+mkdir $HOME/help
 git clone https://github.com/Cordelya/axemoor-bootstrap.git $HOME/help 
 PATH=$PATH:$HOME/help/
-chmod +x axemoor.sh
-echo "Setup is complete. Please type "axemoor.sh" at the command prompt and
-press the Enter key to access the help system"
+chmod +x $HOME/help/axemoor.sh
+echo "Setup is complete. System reboot required.
+
+Once your system reboot is complete, you can test this installation 
+by visiting http://axemoor.net using your local browser. 
+
+Your /etc/hosts file has been modified to direct axemoor.net to 
+the local host. Changes you make to files in 
+
+/var/www/axemoor/public_html 
+
+will appear on refresh(F5). 
+
+Press enter to continue." e
+sudo reboot
